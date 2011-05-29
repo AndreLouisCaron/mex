41131a3d9c2d306e1920bc3dda878cf6b8543f21
Andre Caron <andre.louis.caron@usherbrooke.ca>
2011-05-29 17:04:16 +0000

    Updated copyright notice.

--- mex/error.hpp
+++ mex/error.hpp
@@ -1,7 +1,7 @@
 #ifndef _mex_error_hpp__
 #define _mex_error_hpp__
 
-// Copyright(c) Andre Caron, 2009-2010
+// Copyright(c) Andre Caron, 2009-2011
 //
 // This document is covered by the Artistic License 2.0 (Open Source Initiative
 // approved license). A copy of the license should have been provided alongside
--- mex/exception.hpp
+++ mex/exception.hpp
@@ -1,7 +1,7 @@
 #ifndef _mex_exception_hpp__
 #define _mex_exception_hpp__
 
-// Copyright(c) Andre Caron, 2009-2010
+// Copyright(c) Andre Caron, 2009-2011
 //
 // This document is covered by the Artistic License 2.0 (Open Source Initiative
 // approved license). A copy of the license should have been provided alongside
